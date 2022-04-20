4a4fd2951b9389db6a13036269b02ec8df76e9db
Zapolatero <pascuzzilouis@gmail.com>
2022-04-20 15:22:56 +0200

    replacement of accents in specified files

--- desaccentox.ps1
+++ desaccentox.ps1
@@ -1,12 +1,20 @@
-
 param(
-    [string[]]$Files)
+    [string[]]$Files
+)
 
 process {
-    Write-Host "test"
+    $JSON_PATH = "accents.json";
     foreach ($file in $Files) {
-        $file
-        Get-content $file
-        Write-Host "test"
+        $text = Get-content -Encoding utf8 -Path $file;
+        $AccentsJson = Get-content $JSON_PATH -Encoding utf8 | ConvertFrom-Json;
+        foreach ($accent in $AccentsJson.accents) {
+            if ($text.Contains($accent[0])) {
+                $text = $text.Replace($accent[0], $accent[1])
+            }
+        }
+        Set-Content -Path $file -Value $text -Encoding utf8
+        Write-Host $file "desaccentoxified"
     }
+    Write-Host "All desaccentoxified"
 }
+ 
